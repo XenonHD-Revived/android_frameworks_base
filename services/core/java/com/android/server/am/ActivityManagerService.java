293c8df51a055a5830ceb7a8446c4ad0902a5246
Marzia Favaro <marziana@google.com>
2023-07-31 15:10:34 +0000

    BACKPORT: Require permission to unlock keyguard
    Bug: 288896339
    Test: Manual, verify that the app which can be found on the bug can no longer call
    keyguardGoingAway successfully
    
    Require permission to unlock keyguard
    
    Bug: 288896339
    Test: Manual, verify that the app which can be found on the bug can no longer call
    keyguardGoingAway successfully
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:bd2aa5d309c5bf8e73161975bd5aba7945b25e84)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ef1345d278bd2a8944c6362bf65cff7305ca6fc5)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ad8e7e3b1db22684988a179e23639567a4096ca6)
    Merged-In: I7ba7e56f954c8e6f1f734311f735215918975bc6
    Change-Id: I7ba7e56f954c8e6f1f734311f735215918975bc6
    
    Change-Id: Id9a5ac676c7047c5c4d73b617a1cd628ba885ffe

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -260,6 +260,7 @@ import cyanogenmod.power.PerformanceManagerInternal;
 import libcore.io.IoUtils;
 import libcore.util.EmptyArray;
 
+import static android.Manifest.permission.CONTROL_KEYGUARD;
 import static android.Manifest.permission.INTERACT_ACROSS_USERS;
 import static android.Manifest.permission.INTERACT_ACROSS_USERS_FULL;
 import static android.Manifest.permission.MANAGE_ACTIVITY_STACKS;
@@ -7078,6 +7079,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
     @Override
     public void keyguardGoingAway(int flags) {
+        enforceCallingPermission(CONTROL_KEYGUARD, "keyguardGoingAway()");
         enforceNotIsolatedCaller("keyguardGoingAway");
         final long token = Binder.clearCallingIdentity();
         try {
